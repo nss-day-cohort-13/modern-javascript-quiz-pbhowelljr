f99fab4a5471e7c6196eb05b42356a79a1941193
pbhowelljr <pbhowelljr@gmail.com>
2016-06-05 15:12:56 -0500

    add bootstrap

--- javascripts/domManager.js
+++ javascripts/domManager.js
@@ -7,32 +7,50 @@ var battledome = (function(battledome){
 	//ADDS GAME SETUP VIEW TO DOM//
 	battledome.addGameSetupView = () => {
 		$('.mainContent').html(`
-			<input class="nameInput" type="text" placeholder="player A name:">
-			<select class="playerModel">
-				<option value="Quadcopter">Quadcopter</option>
-				<option value="Hovership">Hovership</option>
-				<option value="LaserMech">LaserMech</option>
-				<option value="GunMech">GunMech</option>
-				<option value="ArmorBuggy">ArmorBuggy</option>
-				<option value="FlameTank">FlameTank</option>
-			</select>
-			<input class="nameInput" type="text" placeholder="player B name:">
-			<select class="playerModel">
-				<option value="Quadcopter">Quadcopter</option>
-				<option value="Hovership">Hovership</option>
-				<option value="LaserMech">LaserMech</option>
-				<option value="GunMech">GunMech</option>
-				<option value="ArmorBuggy">ArmorBuggy</option>
-				<option value="FlameTank">FlameTank</option>
-			</select>
-			<button id="initializeCombatButton" type="submit">Submit</button>
+			<div class='row'>
+				<div class='col-sm-2'></div>
+				<input class="nameInput col-sm-3" type="text" placeholder="player A name:">
+				<div class='col-sm-2'></div>
+				<input class="nameInput col-sm-3" type="text" placeholder="player B name:">
+				<div class='col-sm-2'></div>
+			</div>
+			<div class='row'>	
+				<div class='col-sm-2'></div>
+				<select class="playerModel col-sm-3">
+					<option value="Quadcopter">Quadcopter</option>
+					<option value="Hovership">Hovership</option>
+					<option value="LaserMech">LaserMech</option>
+					<option value="GunMech">GunMech</option>
+					<option value="ArmorBuggy">ArmorBuggy</option>
+					<option value="FlameTank">FlameTank</option>
+				</select>
+				<div class='col-sm-2'></div>
+				<select class="playerModel col-sm-3">
+					<option value="Quadcopter">Quadcopter</option>
+					<option value="Hovership">Hovership</option>
+					<option value="LaserMech">LaserMech</option>
+					<option value="GunMech">GunMech</option>
+					<option value="ArmorBuggy">ArmorBuggy</option>
+					<option value="FlameTank">FlameTank</option>
+				</select>
+				<div class='col-sm-2'></div>
+			</div>
+			<div class='row'>
+				<div class='col-sm-4'></div>
+				<button class='col-sm-4 btn btn-primary' id="initializeCombatButton" type="submit">Submit</button>
+				<div class='col-sm-4'></div>
+			</div>
 			`);
 	};
 
 	//ADDS COMBAT VIEW TO DOM//
 	battledome.addCombatView = () => {
 		$('.mainContent').html(`
-			<button id="attackButton" type="submit">Attack</button>
+			<div class='row'>
+				<div class='col-sm-4'></div>
+				<button  class='col-sm-4 btn btn-primary' id="attackButton" type="submit">Attack</button>
+				<div class='col-sm-4'></div>
+			</div>	
 			<div class="stats"></div>
 			`);
 	};
@@ -40,27 +58,55 @@ var battledome = (function(battledome){
 	//ADDS PLAYERS DATA TO DOM//
 	battledome.initializeStats = (playerA, playerB) => {
 		$('.stats').html(`
-			<p>${playerA.name}: ${playerA.model} ${playerA.type}'s health = ${playerA.health.toFixed(2)}</p>
-			<p>${playerB.name}: ${playerB.model} ${playerB.type}'s health = ${playerB.health.toFixed(2)}</p>
+			<div class='row'>
+				<div class='col-sm-2'></div>
+				<p class='col-sm-3'>${playerA.name}: ${playerA.model} ${playerA.type}'s health = ${playerA.health.toFixed(2)}</p>
+				<div class='col-sm-2'></div>
+				<p class='col-sm-3'>${playerB.name}: ${playerB.model} ${playerB.type}'s health = ${playerB.health.toFixed(2)}</p>
+				<div class='col-sm-2'></div>
+			</div>	
 			`);
 	};
 
 	//UPDATES PLAYERS DATA IN DOM//
 	battledome.updateStats = (playerA, playerB, playerADamage, playerBDamage) => {
 		$('.stats').html(`
-			<p>${playerA.name}: ${playerA.model} ${playerA.type}'s health = ${playerA.health.toFixed(2)}</p>
-			<p>${playerB.name}: ${playerB.model} ${playerB.type}'s health = ${playerB.health.toFixed(2)}</p>
-			<p>${playerA.name} did ${playerADamage.toFixed(2)} damage to ${playerB.name}</p>
-			<p>${playerB.name} did ${playerBDamage.toFixed(2)} damage to ${playerA.name}</p>
+			<div class='row'>
+				<div class='col-sm-2'></div>
+				<p class='col-sm-3'>${playerA.name}: ${playerA.model} ${playerA.type}'s health = ${playerA.health.toFixed(2)}</p>
+				<div class='col-sm-2'></div>
+				<p class='col-sm-3'>${playerB.name}: ${playerB.model} ${playerB.type}'s health = ${playerB.health.toFixed(2)}</p>
+				<div class='col-sm-2'></div>
+			</div>	
+			<div class='row'>
+				<div class='col-sm-2'></div>
+				<p class='col-sm-3'>${playerB.name} did ${playerBDamage.toFixed(2)} damage to ${playerA.name}</p>
+				<div class='col-sm-2'></div>
+				<p class='col-sm-3'>${playerA.name} did ${playerADamage.toFixed(2)} damage to ${playerB.name}</p>
+				<div class='col-sm-2'></div>
+			</div>	
 			`);
 	};
 
 	battledome.endGameView = function(winner, loser){
-		$('.stats').append(`<p>${winner.name}'s ${winner.model} ${winner.type} defeated ${loser.name}'s ${loser.model} ${loser.type}!</p>`);
-		$('.stats').append(`<p>${winner.name} wins!</p>`);
+
+		$('.stats').append(`
+			<div class='row'>
+				<div class='col-sm-3'></div>
+				<p class='col-sm-6'>
+					${winner.name}'s ${winner.model} ${winner.type} defeated ${loser.name}'s ${loser.model} ${loser.type}!
+				</p>
+				<div class='col-sm-3'></div>
+			</div>
+			<div class='row'>
+				<div class='col-sm-3'></div>
+					<p class='col-sm-6'>${winner.name} wins!</p>
+				<div class='col-sm-3'></div>
+			</div>	
+			`);
 	};
 
-
+			
 	//CREATES NEW PLAYERS OBJECTS FROM UI INPUT//
 	battledome.initializeCombatButtonCallback = () => {
 		const aName = $('.nameInput')[0].value;
